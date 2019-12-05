fba029116055d8d2843aa217864f6c3354a99ca4
louis baptiste <louisvis@ymail.com>
2019-12-05 03:59:13 +0000

    Automatically backed up by Learn

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'users', type: 'feature' do
+#describe 'users', type: 'feature' do
 
   before do
     @post = Post.create(title: "Feeling Groovy", content: "I'm feeling so groovy")
